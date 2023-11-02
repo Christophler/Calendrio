611509db97f289c3c52ba0a76c4086a8cb8ca3f1
Christophler <chris.lpher15@gmail.com>
2023-11-01 22:01:26 -0400

    Added functionality for appending events to a dictionary object. Also added .ics files to .gitignore

--- calendar_functions/read_calendar.py
+++ calendar_functions/read_calendar.py
@@ -11,33 +11,52 @@ from pytz import UTC
 # https://www.tutorialspoint.com/downloading-files-from-web-using-python
 import requests
 
+from collections import defaultdict
 
 
 class Calendar():
     print("Test")
     # Class constructor
-    def __init__(self, calendar_url):
+    def __init__(self, calendar_url, event_dict=None):
         self.calendar_url = calendar_url
+        self.event_dict = event_dict
 
     def read_calendar_url(self):
-        r = requests.get(self.calendar_url, allow_redirects=True)
+        # Initializing a dictionary to store all the event important details
+        event_dict = {'Summary': [],
+                      'Description' : [],
+                      'Date Start': [],
+                      'Date End': []}
 
+        # Writing the calendar contents to a file called 'calendar.ics'
+        r = requests.get(self.calendar_url, allow_redirects=True)
         open('calendar_functions/calendar.ics', 'wb').write(r.content)
 
+        # Opening the file and reading it
         cal_ics_file = open('calendar_functions/calendar.ics', 'rb')
-
         calendar = cal.from_ical(cal_ics_file.read())
 
-        # Looping through each event in the calendar
+        # Looping through each event in the calendar and adding it to the dictionary
         for component in calendar.walk():
             if component.name == "VEVENT":
-                print(component.get('SUMMARY').strip())
-                print(component.get('DESCRIPTION').strip())
-                # Printing the datetime object
-                print(component.get('DTSTART').dt)
-                print(component.get('DTEND').dt, "\n")
+                event_dict['Summary'].append(component.get('SUMMARY').strip())
+                event_dict['Description'].append(component.get('DESCRIPTION').strip())
+                event_dict['Date Start'].append(component.get('DTSTART').dt)
+                event_dict['Date End'].append(component.get('DTEND').dt)
 
         cal_ics_file.close()
 
+        # for key in event_dict.keys():
+        #     print("Key: ", key)
+        #     for value in event_dict[key]:
+        #         print("Value: ", value)
 
+        return event_dict
 
+    # Setting the dictionary
+    def set_event_dict(self):
+        self.event_dict = self.read_calendar_url()
+
+    def get_event_dict(self):
+        self.set_event_dict()
+        return self.event_dict
--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def main():
     calendar_url = input("Enter the calendar download url: ")
 
     calendar = Calendar(calendar_url)
-    
-    calendar.read_calendar_url()
+
+    event_dict = calendar.get_event_dict()
+
+    # for key in event_dict.keys():
+    #     print("Key: ", key)
+    #     for value in event_dict[key]:
+    #         print("Value: ", value)
 
 main()
