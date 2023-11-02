e8d0a06dc0e1de601442a9cddf7df2bc7b13fa78
Christophler <chris.lpher15@gmail.com>
2023-11-01 21:17:31 -0400

    Added calendar reading functionality. Currently only prints the events to the terminal.

--- calendar_functions/read_calendar.py
+++ calendar_functions/read_calendar.py
@@ -3,12 +3,15 @@
 # https://realpython.com/python-pep8/
 
 # https://stackoverflow.com/questions/3408097/parsing-files-ics-icalendar-using-python
-from icalendar import Calendar, Event
+from icalendar import Calendar as cal
+from icalendar import Event
 from datetime import datetime
 from pytz import UTC
 
 # https://www.tutorialspoint.com/downloading-files-from-web-using-python
 import requests
+
+
 
 class Calendar():
     print("Test")
@@ -20,6 +23,21 @@ class Calendar():
         r = requests.get(self.calendar_url, allow_redirects=True)
 
         open('calendar_functions/calendar.ics', 'wb').write(r.content)
+
+        cal_ics_file = open('calendar_functions/calendar.ics', 'rb')
+
+        calendar = cal.from_ical(cal_ics_file.read())
+
+        # Looping through each event in the calendar
+        for component in calendar.walk():
+            if component.name == "VEVENT":
+                print(component.get('SUMMARY').strip())
+                print(component.get('DESCRIPTION').strip())
+                # Printing the datetime object
+                print(component.get('DTSTART').dt)
+                print(component.get('DTEND').dt, "\n")
+
+        cal_ics_file.close()
 
 
 
